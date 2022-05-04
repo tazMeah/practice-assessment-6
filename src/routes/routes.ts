9858b3b92500b4260d792e195d3c8564a7b9fb26
Montaz Meah II <taz@MEAHs-iMac.local>
2022-05-04 17:43:52 -0400

    setup

---
+++ src/routes/routes.ts
@@ -0,0 +1,22 @@
+// require the express module
+
+ 
+
+// 1. GET: /pokemon - shows all Pokemon
+
+
+// 2. GET: /pokemon/:id - shows the given pokemon
+
+
+// 3. GET: /pokemon -  using a "minAttack" query allow the user to see all the pokemon with an attack value higher than the minAttack
+
+
+
+// 4. POST: /pokemon - allow the user to add a new pokemon using the request body. Be sure the id is unique (not already used).
+
+
+// Test all your endpoints in Postman
+
+
+// Extended challenge: allow updating and deleting of pokemon.
+
